e746c0ea550fc48e87292bb07fae1af4e313926d
Greg Harris <ghart.productions@gmail.com>
2021-05-21 00:20:22 -0700

    added NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <p>Hello World</p>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function NavBar() {
+
+return(
+    <header>
+    <nav class="navbar navbar-expand-lg top-fixed bg-dark navbar-dark">
+        <a class="navbar-brand">Greg Harris</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="projects.html">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="portfolio.html">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <span class="navbar-text text-primary" id="Resume">
+            <a href="./assets/Greg Harris - Resume.pdf">View My Resume</a>
+        </span>
+    </nav>
+</header>
+)
+
+}
+
+export default NavBar;
