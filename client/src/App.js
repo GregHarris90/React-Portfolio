20f8ad5577649a72c14dcb7a8328a54a962aa4e5
Greg Harris <ghart.productions@gmail.com>
2021-05-22 21:29:53 -0700

    fixed the background image

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Project from "./pages/Project";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
+import "./styles.css";
 
 function App() {
   return (
