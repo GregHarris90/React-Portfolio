6ef5f05eacf9ec4ff0969370ada800a1ad9552c5
Greg Harris <ghart.productions@gmail.com>
2021-05-23 00:32:05 -0700

    built Photos component

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -7,8 +7,8 @@ function Project(props) {
             <div className="container card" id="project">
                 <div className="card-deck">
 
-                    {props.projects.map(item => (<div className="card">
-                        <img src={item.image} className="card-img-top" alt="Group-Project-1" />
+                    {props.projects.map(item => (<div className="card" key={item.id}>
+                        <img src={item.image} className="card-img-top" alt={item.id} />
                         <div className="card-body">
                             <h5 className="card-title">{item.title}</h5>
                             <p className="card-text">{item.description}</p>
