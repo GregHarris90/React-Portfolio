8b7fdafc5c02a1356f466758a823142563e8b8f3
Greg Harris <ghart.productions@gmail.com>
2021-05-25 00:23:37 -0700

    created photo component and added it to gallery page

--- client/src/components/Photos.js
+++ client/src/components/Photos.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 function Photos() {
-    
-    return(
-        <div>
 
+    return (
+        <div>
+            <div class="container card body">
+                <h2 class="text-uppercase text-primary">Image Gallery:</h2>
+
+                <p class="font-italic">Click to enlarge images!</p>
+
+                <div class="row">
+
+                    <div class="col-md-4">
+
+                        <div class="thumbnail">
+                            <a href={props.link}
+                                target="_blank">
+                                <img src={props.image}
+                                    alt={props.name} />
+                                <div class="caption" />
+                                <p>{props.description}</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Photos from "../components/Photos";
 
 function Gallery() {
     return(
         <div>
-            
+            <Photos />
         </div>
     )
 }
