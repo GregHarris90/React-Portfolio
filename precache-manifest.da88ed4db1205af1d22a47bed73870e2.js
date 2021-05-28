2f87d7a429737ca47f1e4fb44ea4c95ed27e6677
Greg Harris <ghart.productions@gmail.com>
2021-05-28 00:01:09 -0700

    Updates

---
+++ precache-manifest.da88ed4db1205af1d22a47bed73870e2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ebbd7b60da488581df",
+    "url": "/React-Portfolio/static/css/main.8a6d00c9.chunk.css"
+  },
+  {
+    "revision": "99ebbd7b60da488581df",
+    "url": "/React-Portfolio/static/js/main.316707fe.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "0dba3d132207a6120918",
+    "url": "/React-Portfolio/static/css/2.1469a4c5.chunk.css"
+  },
+  {
+    "revision": "0dba3d132207a6120918",
+    "url": "/React-Portfolio/static/js/2.46558ed8.chunk.js"
+  },
+  {
+    "revision": "7dff0b67f24a79f50890dc1aa610c652",
+    "url": "/React-Portfolio/static/media/Wedding.7dff0b67.jpg"
+  },
+  {
+    "revision": "c229e126f1fef03700ad02dc4ce26585",
+    "url": "/React-Portfolio/static/media/Family_1.c229e126.jpg"
+  },
+  {
+    "revision": "b5bb9e8ac9639283c44f0ae7e6eb2041",
+    "url": "/React-Portfolio/static/media/Alaska_1.b5bb9e8a.jpg"
+  },
+  {
+    "revision": "598b7d44f12fa932fb01bdbfddb70ed6",
+    "url": "/React-Portfolio/static/media/20180814_111606_001.598b7d44.jpg"
+  },
+  {
+    "revision": "528c570170e651b862af5f320a8eda40",
+    "url": "/React-Portfolio/static/media/20180815_093743.528c5701.jpg"
+  },
+  {
+    "revision": "9cefef0f39a1c9f47d4c946ab1fb0f8f",
+    "url": "/React-Portfolio/static/media/37867850_10214106848280103_7432809272188076032_o.9cefef0f.jpg"
+  },
+  {
+    "revision": "4a2ad5c5bd104f2bcad0880d52af8919",
+    "url": "/React-Portfolio/static/media/gyrgyrhgrh 071.4a2ad5c5.jpg"
+  },
+  {
+    "revision": "e384ce9ceca3f0c07ad63b565d02b222",
+    "url": "/React-Portfolio/static/media/66384523_10219554202076845_4210518373420761088_n.e384ce9c.jpg"
+  },
+  {
+    "revision": "ee91ba8603271860fc1185b6795a6df6",
+    "url": "/React-Portfolio/static/media/102764614_707632373355883_5832740373946712142_o.ee91ba86.jpg"
+  },
+  {
+    "revision": "7681c89f62eb011b5d5901504d24af26",
+    "url": "/React-Portfolio/static/media/Greg Harris - Resume.7681c89f.pdf"
+  },
+  {
+    "revision": "273b2cc680259e78b02d8a91fa982759",
+    "url": "/React-Portfolio/static/media/gyrgyrhgrh 028.273b2cc6.jpg"
+  },
+  {
+    "revision": "8edd8a866dd7df0f73f6ddc33ceef77f",
+    "url": "/React-Portfolio/static/media/GP_1.8edd8a86.png"
+  },
+  {
+    "revision": "dc38bcc7efbf694bb34f7566f56ec5ea",
+    "url": "/React-Portfolio/static/media/MP_2.dc38bcc7.png"
+  },
+  {
+    "revision": "d09cef47e4a64215f10765afd878b719",
+    "url": "/React-Portfolio/static/media/MP_1.d09cef47.png"
+  },
+  {
+    "revision": "df89ac616e477673cf721c1fc555495c",
+    "url": "/React-Portfolio/static/media/YNC-HomePage.df89ac61.png"
+  },
+  {
+    "revision": "be04b8a5fe51c02bae19fb743e7ae143",
+    "url": "/React-Portfolio/static/media/TPG.be04b8a5.png"
+  },
+  {
+    "revision": "69c2c83ffe82a03f84e7b796df793bb8",
+    "url": "/React-Portfolio/static/media/ECB.69c2c83f.png"
+  },
+  {
+    "revision": "3d900b462a83942566ba2f0a61bc700d",
+    "url": "/React-Portfolio/static/media/276.3d900b46.jpg"
+  },
+  {
+    "revision": "0deb63461d5a0d7b78f41acc93967f71",
+    "url": "/React-Portfolio/index.html"
+  }
+];
