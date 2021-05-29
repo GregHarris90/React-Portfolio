fbadd341b3b462b4ce0c9d86fc82738343dcc20d
Greg Harris <ghart.productions@gmail.com>
2021-05-28 20:26:38 -0700

    added links to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Welcome from "../components/Welcome";
+import Links from "../components/Links";
 import About from "../components/About";
 
 function Home() {
     return(
         <div>
             <Welcome />
+            <Links />
             <About />
         </div>
     )
